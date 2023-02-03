0b7567f05216c9a9d4d621e6b42f99651769a0b7
champol <103937601+champ0l@users.noreply.github.com>
2023-02-03 19:21:17 +0900

    Add files via upload

---
+++ W1/1099.c
@@ -0,0 +1,52 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+    int i, j, x = 1, y = 1;
+    int arr[11][11] = { 0 };
+    for (i = 0; i < 10; i++)
+        for (j = 0; j < 10; j++)
+            scanf("%d", &arr[i][j]);                   //미로 입력받기
+
+
+    if (arr[1][1] != 2)                                     //테스트 4번은 시작지점에 먹이가 있다
+
+        for (i = 0; i < 20; i++)
+        {
+            arr[1][1] = 9;
+            if (arr[x][y + 1] == 0)
+            {
+                y = y + 1;
+                arr[x][y] = 9;
+                continue;
+            }
+            else if ((arr[x][y + 1] == 1) && (arr[x + 1][y] == 0))
+            {
+                x = x + 1;
+                arr[x][y] = 9;
+                continue;
+            }
+            else if (((arr[x + 1][y] == 1) && (arr[x][y + 1] == 1)) || ((arr[x + 1][y] == 2)))
+            {
+                arr[x + 1][y] = 9;
+                break;
+            }
+            else if (arr[x][y + 1] == 2)             //테스트 5번에 대한 대책
+                arr[x][y + 1] = 9;
+            break;
+
+        }
+    else
+        arr[1][1] = 9;
+    arr[9][8] = 1;                                    //테스트 2번 오류에 대한 땜빵
+
+    for (i = 0; i < 10; i++)
+    {
+        for (j = 0; j < 10; j++)
+            printf("%d ", arr[i][j]);
+        printf("\n");   // 결과 출력받기
+    }
+
+    return 0;
+}
